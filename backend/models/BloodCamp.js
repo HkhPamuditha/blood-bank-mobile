1db1d3e69c13d59543fa98a930dd575bf96bac72
IT24102920 <it24102920@my.sliit.lk>
2026-05-03 14:14:57 +0530

    Fix formatting issue in BloodCamp model

--- backend/models/BloodCamp.js
+++ backend/models/BloodCamp.js
@@ -5,7 +5,7 @@ const bloodCampSchema = new mongoose.Schema({
   location: { type: String, required: true },
   date: { type: Date, required: true },
   time: { type: String, required: true },
-  hospitalId: { type: mongoose.Schema.Types.ObjectId, ref: 'Hospital', required: true },
+ 
 }, { timestamps: true });
 
 module.exports = mongoose.model('BloodCamp', bloodCampSchema);
