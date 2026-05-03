eadcb4403cd6d0a2e104692e789a7020fd895ddd
IT24100440 <it24100440@my.sliit.lk>
2026-05-04 00:22:51 +0530

    Update Hospital.js
    edit in hospital.js

--- backend/models/Hospital.js
+++ backend/models/Hospital.js
@@ -1,6 +1,12 @@
 const mongoose = require('mongoose');
 
 const hospitalSchema = new mongoose.Schema({
+
+ 
+ hospitalName: { type: String, required: true },
+  location: { type: String, required: true },
+  contactNumber: { type: String, required: true },
+}, { timestamps: true });
  
 
 module.exports = mongoose.model('Hospital', hospitalSchema);
