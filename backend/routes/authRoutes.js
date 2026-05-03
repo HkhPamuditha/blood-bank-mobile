179b9ff15285f23f4e5690955987d55dbfe62aae
IT24100440 <it24100440@my.sliit.lk>
2026-05-04 00:25:26 +0530

    Update authRoutes.js

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const { loginUser, getMe } = require('../controllers/authController');
 const { protect } = require('../middleware/authMiddleware');
 
-router.post('/login', loginUser);
+
 router.get('/me', protect, getMe);
 
 module.exports = router;
