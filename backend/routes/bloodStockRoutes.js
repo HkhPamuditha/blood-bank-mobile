48e5d03fa42b450d1ec715c180b370be2cac178d
IT24102575 <IT24102575@my.sliit.lk>
2026-05-05 00:32:28 +0530

    Update bloodStockRoutes.js

--- backend/routes/bloodStockRoutes.js
+++ backend/routes/bloodStockRoutes.js
@@ -1,8 +1,9 @@
+// Import Express framework
 const express = require('express');
 const router = express.Router();
 const { getBloodStock, updateBloodStock } = require('../controllers/bloodStockController');
 const { protect } = require('../middleware/authMiddleware');
 
 router.route('/').get(protect, getBloodStock).post(protect, updateBloodStock);
-
+// Export router to use in main server file
 module.exports = router;
